f5c82ddaffacc2c110b71f9560d770f3dc07379d
KokaKiwi <kokakiwi@kokakiwi.net>
2014-11-10 17:56:43 +0100

    First commit.

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = {};
+searchIndex['markovgen'] = {"items":[],"paths":[]};
+searchIndex['markov'] = {"items":[[0,"","markov",""],[1,"MarkovGenerator","",""],[11,"cache","","",0],[11,"words","","",0],[6,"Cache","",""],[9,"put","","",1],[9,"get","","",1],[10,"has","","",1],[10,"put","std::collections::hash::map","",2],[10,"get","","",2],[10,"new","markov","",0],[10,"feed_from_words","","",0],[10,"feed_from_file","","",0],[10,"generate_text","","",0]],"paths":[[1,"MarkovGenerator"],[6,"Cache"],[1,"HashMap"]]};
+
+initSearch(searchIndex);
